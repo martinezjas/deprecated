abe57475cfc281643a0cb2ff5cca22d45967c297
Jason Martinez <66093119+martinezjas@users.noreply.github.com>
2023-06-13 02:23:29 -0500

    Changed homeindex test

--- e2e/homeindex.spec.ts
+++ e2e/homeindex.spec.ts
@@ -1,128 +1,16 @@
-import {test, expect, chromium, firefox, webkit, Browser, Page} from '@playwright/test';
-import { defineComponent } from 'vue';
+import { test, expect } from '@playwright/test';
 
-describe('HomeIndex.vue', () => {
-  let browser: Browser;
-  let page: Page;
+test('HomeIndex.vue', async ({ page, browserName }) => {
+  await page.goto('http://localhost:8080');
 
-  beforeAll(async () => {
-    browser = await chromium.launch();
-  });
+  // Expect the profile picture to be present.
+  const profilePicture = await page.$('img[alt="Profile picture"]');
+  expect(profilePicture).toBeTruthy();
 
-  afterAll(async () => {
-    await browser.close();
-  });
+  // Expect the GitHub and LinkedIn links to be present.
+  const githubLink = await page.$('a[href="https://github.com/martinezjas"]');
+  expect(githubLink).toBeTruthy();
 
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto('http://localhost:3000');
-  });
-
-  afterEach(async () => {
-    await page.close();
-  });
-
-  it('should display the title', async () => {
-    const title = await page.$eval('h1', (el) => el.textContent);
-    expect(title).toBe('folfmart');
-  });
-
-  it('should display the profile picture', async () => {
-    const profilePicture = await page.$('img[alt="Profile picture"]');
-    expect(profilePicture).toBeTruthy();
-  });
-
-  it('should display the GitHub link', async () => {
-    const githubLink = await page.$('a[href="https://github.com/martinezjas"]');
-    expect(githubLink).toBeTruthy();
-  });
-
-  it('should display the LinkedIn link', async () => {
-    const linkedinLink = await page.$('a[href="https://linkedin.com/in/martinezjas"]');
-    expect(linkedinLink).toBeTruthy();
-  });
-});
-
-describe('HomeIndex.vue in Firefox', () => {
-  let browser: Browser;
-  let page: Page;
-
-  beforeAll(async () => {
-    browser = await firefox.launch();
-  });
-
-  afterAll(async () => {
-    await browser.close();
-  });
-
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto('http://localhost:3000');
-  });
-
-  afterEach(async () => {
-    await page.close();
-  });
-
-  it('should display the title', async () => {
-    const title = await page.$eval('h1', (el) => el.textContent);
-    expect(title).toBe('folfmart');
-  });
-
-  it('should display the profile picture', async () => {
-    const profilePicture = await page.$('img[alt="Profile picture"]');
-    expect(profilePicture).toBeTruthy();
-  });
-
-  it('should display the GitHub link', async () => {
-    const githubLink = await page.$('a[href="https://github.com/martinezjas"]');
-    expect(githubLink).toBeTruthy();
-  });
-
-  it('should display the LinkedIn link', async () => {
-    const linkedinLink = await page.$('a[href="https://linkedin.com/in/martinezjas"]');
-    expect(linkedinLink).toBeTruthy();
-  });
-});
-
-describe('HomeIndex.vue in WebKit', () => {
-  let browser: Browser;
-  let page: Page;
-
-  beforeAll(async () => {
-    browser = await webkit.launch();
-  });
-
-  afterAll(async () => {
-    await browser.close();
-  });
-
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto('http://localhost:3000');
-  });
-
-  afterEach(async () => {
-    await page.close();
-  });
-
-  it('should display the title', async () => {
-    const title = await page.$eval('h1', (el) => el.textContent);
-    expect(title).toBe('folfmart');
-  });
-
-  it('should display the profile picture', async () => {
-    const profilePicture = await page.$('img[alt="Profile picture"]');
-    expect(profilePicture).toBeTruthy();
-  });
-
-  it('should display the GitHub link', async () => {
-    const githubLink = await page.$('a[href="https://github.com/martinezjas"]');
-    expect(githubLink).toBeTruthy();
-  });
-
-  it('should display the LinkedIn link', async () => {
-    const linkedinLink = await page.$('a[href="https://linkedin.com/in/martinezjas"]');
-    expect(linkedinLink).toBeTruthy();
-  });
+  const linkedinLink = await page.$('a[href="https://linkedin.com/in/martinezjas"]');
+  expect(linkedinLink).toBeTruthy();
 });
